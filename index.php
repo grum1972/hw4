64dc8cdaf48d76fb45961a5397825d16335814f5
Kotov Oleg <grum1972@gmail.com>
2020-06-18 22:20:27 +0300

    Final version!

--- index.php
+++ index.php
@@ -4,111 +4,18 @@
 // Интерфейс
 interface iTariff
 {
-
     public function getTime();
 
     public function getPrice();
 
     public function getDescription();
-
 }
 
-class BasicTariff implements iTariff
+// Абстрактный класс услуги
+abstract class Service implements iTariff
 {
-    protected $distance;
-    protected $time;
-    protected $pricePerKm = 10;
-    protected $pricePerMin = 3;
-
-    public function __construct(int $distance, int $time)
-    {
-        $this->time = $time;
-        $this->distance = $distance;
-    }
-
-    public function getPrice()
-    {
-        return $this->distance * $this->pricePerKm + $this->time * $this->pricePerMin;
-    }
-
-    public function getTime()
-    {
-        return $this->time;
-    }
-
-    public function getDescription()
-    {
-        return 'Базовый';
-    }
-}
-class StudentTariff implements iTariff
-{
-    protected $distance;
-    protected $time;
-    protected $pricePerKm = 4;
-    protected $pricePerMin = 1;
-
-    public function __construct(int $distance, int $time)
-    {
-        $this->time = $time;
-        $this->distance = $distance;
-    }
-
-    public function getPrice()
-    {
-        return $this->distance * $this->pricePerKm + $this->time * $this->pricePerMin;
-    }
-
-    public function getTime()
-    {
-        return $this->time;
-    }
-
-    public function getDescription()
-    {
-        return 'Студенческий';
-    }
-}
-class HourTariff implements iTariff
-{
-    protected $distance;
-    protected $time;
-    protected $pricePerHour = 200;
-
-    public function __construct(int $distance, int $time)
-    {
-        $this->time = $time;
-        $this->distance = $distance;
-    }
-
-    public function getTime()
-    {
-        return $this->time;
-    }
-
-    function getHour()
-    {
-        //$this->time = $this->tariff->getTime();
-        return ($this->time <= 60) ? 1 : ceil($this->time / 60);
-    }
-
-    public function getPrice()
-    {
-        return $this->getHour()*$this->pricePerHour;
-    }
-
-
-
-    public function getDescription()
-    {
-        return 'Почасовой';
-    }
-}
-
-class ServiceOptDrive implements iTariff
-{
-
     protected $tariff;
+    protected $time;
 
     public function __construct(iTariff $tariff)
     {
@@ -119,7 +26,79 @@ class ServiceOptDrive implements iTariff
     {
         return $this->tariff->getTime();
     }
+}
 
+//Абстрактный класс Тарифа
+abstract class Tariff implements iTariff
+{
+    protected $distance;
+    protected $time;
+    protected $pricePerKm = 0;
+    protected $pricePerMin = 0;
+
+    public function __construct(int $distance, int $time)
+    {
+        $this->time = $time;
+        $this->distance = $distance;
+    }
+
+    public function getPrice()
+    {
+        return $this->distance * $this->pricePerKm + $this->time * $this->pricePerMin;
+    }
+
+    public function getTime()
+    {
+        return $this->time;
+    }
+
+
+}
+
+class BasicTariff extends Tariff
+{
+    protected $pricePerKm = 10;
+    protected $pricePerMin = 3;
+
+    public function getDescription()
+    {
+        return 'Базовый';
+    }
+}
+
+class StudentTariff extends Tariff
+{
+    protected $pricePerKm = 4;
+    protected $pricePerMin = 1;
+
+    public function getDescription()
+    {
+        return 'Студенческий';
+    }
+}
+
+class HourTariff extends Tariff
+{
+    protected $pricePerHour = 200;
+
+    function getHour()
+    {
+        return ($this->time <= 60) ? 1 : ceil($this->time / 60);
+    }
+
+    public function getPrice()
+    {
+        return $this->getHour() * $this->pricePerHour;
+    }
+
+    public function getDescription()
+    {
+        return 'Почасовой';
+    }
+}
+
+class ServiceOptDrive extends Service
+{
     public function getPrice()
     {
         return $this->tariff->getPrice() + 100;
@@ -131,20 +110,8 @@ class ServiceOptDrive implements iTariff
     }
 }
 
-class ServiceGPS implements iTariff
+class ServiceGPS extends Service
 {
-    protected $tariff;
-    protected $time;
-
-    public function __construct(iTariff $tariff)
-    {
-        $this->tariff = $tariff;
-    }
-
-    public function getTime()
-    {
-        return $this->tariff->getTime();
-    }
 
     function getHour()
     {
@@ -165,7 +132,7 @@ class ServiceGPS implements iTariff
 
 echo 'Поездка по тарифу ';
 // Добавляем базовый тариф
-$trip = new BasicTariff(5, 60);
+$trip = new BasicTariff(5, 90);
 echo $trip->getDescription();
 
 // Добавляем дополнительного водителя
@@ -193,7 +160,7 @@ echo '<br>';
 echo 'Поездка по тарифу ';
 
 // Добавляем студенческий тариф
-$trip = new StudentTariff(5, 90);
+$trip = new StudentTariff(10, 50);
 echo $trip->getDescription();
 
 // Добавляем GPS
